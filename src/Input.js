9de76d8017ba93817c3a63a634f85aa3f60817e0
kelkip2001@gmail.com <kelkip2001@gmail.com>
2021-09-17 16:22:19 +0300

    all changes

--- src/Input.js
+++ src/Input.js
@@ -3,6 +3,7 @@ import clsx from "clsx"
 
 export default function Input(props){
     const {type="text",placeholder,name,required,className,...rest}=props
+    const classes = clsx()
     
     return <>
     <input type={type} placeholder={placeholder} required={required} {...rest}/>
