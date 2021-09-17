9b6e36401e316ef764a77c0585bb10be97d67753
kelkip2001@gmail.com <kelkip2001@gmail.com>
2021-09-17 23:33:02 +0300

    build stage
    build folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react"
+import {Route,Switch} from "react-router-dom"
 import Button from "./Button.js"
 import Input from "./Input.js"
 import Uikit from "./Uikit"
-import Navbar from "./Navbar.js"
+import Navbar from "./Navbar.js";
+import Home from "./Home.js"
 
 export default function App(){
   return (<>
   <Navbar/>
+  <Switch>
+  <Route>
+  <Home />
+  </Route>
+  </Switch>
   </>)
 }
