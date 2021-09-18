1128c7dfd7a7f5d76e65ae45e29408a295fc7ee4
kelkip2001@gmail.com <kelkip2001@gmail.com>
2021-09-18 10:47:22 +0300

    update 3

--- src/Products.js
+++ src/Products.js
@@ -1,20 +1,41 @@
 
+import React,{useEffect,useState} from "react";
 import useFetch from "./useFetch.js";
-import {useState,useEffect} from "react"
-
+  import Loader from "./Loader.js";
+  import Product from "./Product.js"
 
 export default function Products(){
-    const {get} = useFetch("https://react-tutorial-demo.firebaseio.com/")
-    const [Products,setProducts]=useState([])
+    const {get,loading} = useFetch("https://react-tutorial-demo.firebaseio.com/")
+    const [products,setProducts]=useState([])
 
     useEffect(()=>{
 get("supermarket.json").then(data=>{
+
     setProducts(data)
-    console.log(data)
-})
-    },[])
-    return <div class="products-layout">
-  <h1>Products..</h1>
-  <p>Take a look at our products</p>
-</div>
+console.log(products)
+  }).catch(e=>{
+    console.log(e)
+  })
+  },[])
+  
+  return (
+    <div className="products-layout">
+      <h1>Products ({products.length})</h1>
+      <p>Take a look at our products </p>
+      <div className="products-grid">
+        {loading && <Loader />}
+     {products.map(product=>{
+       return (<>
+       <Product details={product}/>
+       </>)
+     })}
+     
+          
+          
+          
+        
+
+      </div>
+    </div>
+  );
 }
